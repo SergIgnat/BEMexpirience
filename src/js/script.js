55c585aa8191dc49e1f9958600387731ff38ccc3
Immortal]13 <immortal230719@gmail.com>
2019-10-09 01:28:41 +0400

    making tabs: first tabItem added

--- src/js/script.js
+++ src/js/script.js
@@ -2,16 +2,17 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 500,
         adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../img/slider/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../img/slider/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.svg"></button>',
         responsive: [
             {
               breakpoint: 992,
               settings: {
-                dots: true,
-                arrows: false
+                arrows: false,
+                dots: true
               }
             }
         ]
     });
   });
+
