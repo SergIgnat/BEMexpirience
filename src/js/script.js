9b73082e25ae2f0840d75f950cb89990238db584
Immortal]13 <immortal230719@gmail.com>
2019-10-09 05:13:15 +0400

    Tabs already done with scripts

--- src/js/script.js
+++ src/js/script.js
@@ -14,5 +14,39 @@ $(document).ready(function(){
             }
         ]
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+    });
+
+    // $('.catalog-item__link').each(function(i){
+    // 	$(this).on('click', function(e){
+	// 		e.preventDefault();
+	// 		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+	// 		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+    // 	})
+	// })
+	
+	// $('.catalog-item__link-back').each(function(i){
+    // 	$(this).on('click', function(e){
+	// 		e.preventDefault();
+	// 		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+	// 		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+    // 	})
+	// })
+	
+	function toggleSlide(item) {
+		$(item).each(function(i) {
+			$(this).on('click', function(e){
+				e.preventDefault();
+				$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+				$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active')
+			})
+		})
+	}
+	toggleSlide('.catalog-item__link-back');
+	toggleSlide('.catalog-item__link');
   });
 
