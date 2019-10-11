623d007af1183d7cbbee5b0809db56d8265432d3
Immortal]13 <immortal230719@gmail.com>
2019-10-11 12:19:52 +0400

    Dots done, media to footer done, fixed buttons

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 500,
         adaptiveHeight: true,
+        dotsClass: 'my-dots',
         prevArrow: '<button type="button" class="slick-prev"><img src="img/slider/left.svg"></button>',
         nextArrow: '<button type="button" class="slick-next"><img src="img/slider/right.svg"></button>',
         responsive: [
